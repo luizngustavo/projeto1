ed5b7ea2e155cdbf9b7236d902aac760c5d10524
luizngustavo <luiz-gustavo8@hotmail.com>
2023-05-16 19:39:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from "./componentes/Nav";
+import Produtos from "./componentes/Produtos";
+
+function App  ()  {
+  return (
+
+  <div className="Divbar"><Nav/></div>,
+
+  <div className="produtosHome"><Produtos/></div>
+
+  )
+};
+
+export default App;
