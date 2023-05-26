8901098ea29635d47fd74bc3cec16723e16e0263
Lucas Câmara <lucascamara@outlook.com.br>
2023-05-26 17:00:09 -0300

    update db config

--- src/DB/db.js
+++ src/DB/db.js
@@ -1,21 +1,21 @@
-import pg from "pg";
-const { Pool } = pg;
+var Pool = require("pg-pool");
 
-
-const databaseConfig = { 
-  
-  user: 'postgres',
-  host: 'localhost',
-  database: 'projeto1',
-  password: '123',
-  port: 5432 
+const dbConfig = {
+  database: "projeto1",
+  user: "postgres",
+  password: "123",
+  port: 5432,
+  ssl: true,
+  max: 10, // set pool max size to 10
+  idleTimeoutMillis: 1000, // close idle clients after 1 second
+  connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+  maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
 };
 
-const pool = new Pool(databaseConfig);
+// you can pass properties to the pool
+// these properties are passed unchanged to both the node-postgres Client constructor
+// and the node-pool (https://github.com/coopernurse/node-pool) constructor
+// allowing you to fully configure the behavior of both
+const pool = new Pool(dbConfig);
 
 export default pool;
-
-
-
-
-
